b268d4e621f6803f6f04d011de55fb079929ea80
esther7832 <esther7832@naver.com>
2022-06-04 12:18:51 +0900

    [design] add foodNutInfo page

---
+++ app/src/main/java/kr/ac/cnu/computer/foodpedia_app/FoodNutritionInfoActivity.java
@@ -0,0 +1,18 @@
+package kr.ac.cnu.computer.foodpedia_app;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FoodNutritionInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodnutritioninfo);
+
+
+    }
+}
