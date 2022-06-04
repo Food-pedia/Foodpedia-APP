5592bca0196f7f1526594b97e921b933d417c1c6
esther7832 <esther7832@naver.com>
2022-06-04 18:57:39 +0900

    [feat] add buttons based on the number of foods

--- app/src/main/java/kr/ac/cnu/computer/foodpedia_app/FoodRecognitionActivity.java
+++ app/src/main/java/kr/ac/cnu/computer/foodpedia_app/FoodRecognitionActivity.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 
 public class FoodRecognitionActivity extends AppCompatActivity {
@@ -18,19 +21,36 @@ public class FoodRecognitionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_foodrecognition);
 
         ImageView ivImage = findViewById(R.id.imageView);
-        Button foodButton = findViewById(R.id.food1);
+        LinearLayout foodButtonLayout = findViewById(R.id.foodButtonLayout);
 
-        byte[] byteArray = getIntent().getByteArrayExtra("image");
-        Bitmap image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-        ivImage.setImageBitmap(image);
+        int foodNum = 2;    //인식된 식품 개수 !수정필요
+        Button[] foodButtons = new Button[foodNum]; //인식된 식품 버튼 저장할 배열
+        String[] foodname = {"김치전", "김치찌개"};    //인식된 식품 이름 -> 배열로 되어야할듯 !수정필요
+        for(int i=0; i<foodNum; i++){
+            //인식된 식품 개수만큼 버튼 생성
+            foodButtons[i] = new Button(this);
+            foodButtons[i].setText(foodname[i]);
+            foodButtonLayout.addView(foodButtons[i]);
 
-        foodButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), FoodNutritionInfoActivity.class);
-                startActivity(intent);
-            }
-        });
+            //식품 버튼 누르면 해당 식품영양정보 페이지로 이동
+            Button foodButton = foodButtons[i];
+            foodButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), FoodNutritionInfoActivity.class);
+                    intent.putExtra("foodName",foodButton.getText());   //다음 페이지로 식품 이름 전달
+                    startActivity(intent);
+                }
+            });
+        }
+
+//        //이미지 출럭 -> 모델에서 전달받은 이미지로 수정해야함. !수정필요
+//        byte[] byteArray = getIntent().getByteArrayExtra("image");
+//        Bitmap image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+//        ivImage.setImageBitmap(image);
+
+
+
 
     }
 
