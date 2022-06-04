1c7363ee0a2b588c9a80a4efd34aea4e205c67d6
esther7832 <esther7832@naver.com>
2022-06-04 12:54:37 +0900

    [feat] connect foodBtn with foodNutInfo page

--- app/src/main/java/kr/ac/cnu/computer/foodpedia_app/FoodRecognitionActivity.java
+++ app/src/main/java/kr/ac/cnu/computer/foodpedia_app/FoodRecognitionActivity.java
@@ -1,9 +1,12 @@
 package kr.ac.cnu.computer.foodpedia_app;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -14,10 +17,21 @@ public class FoodRecognitionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodrecognition);
 
+        ImageView ivImage = findViewById(R.id.imageView);
+        Button foodButton = findViewById(R.id.food1);
+
         byte[] byteArray = getIntent().getByteArrayExtra("image");
         Bitmap image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-
-        ImageView ivImage = findViewById(R.id.imageView);
         ivImage.setImageBitmap(image);
+
+        foodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FoodNutritionInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
