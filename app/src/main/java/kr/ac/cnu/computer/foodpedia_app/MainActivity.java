0063ce0978f16a1e5b4c1a75529451cd302a5dac
moo-nerim <ansgpfla7@naver.com>
2022-09-22 17:01:50 +0900

    [feat] 기존 기능 한 레이아웃으로 병합

--- app/src/main/java/kr/ac/cnu/computer/foodpedia_app/MainActivity.java
+++ app/src/main/java/kr/ac/cnu/computer/foodpedia_app/MainActivity.java
@@ -1,26 +1,36 @@
 package kr.ac.cnu.computer.foodpedia_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Layout;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
@@ -37,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
 
     TextView userName;
     ImageView profile;
+    Button bloodBtn, btn_camera, btn_gallery;
+    final static int TAKE_PICTURE = 1;
+    final static int GET_FROM_GALLERY = 2;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
         profile = findViewById(R.id.user_profile);
         userName = findViewById(R.id.user_text);
+        bloodBtn = findViewById(R.id.bloodBtn);
+        btn_camera = findViewById(R.id.btn_camera);
+        btn_gallery = findViewById(R.id.btn_gallery);
 
         // ***** 카카오 프로필 이미지 *****
         String url = ((GlobalApplication) getApplication()).getKakaoProfile();
@@ -56,6 +73,38 @@ public class MainActivity extends AppCompatActivity {
         Log.e("카카오 이름 : "," "+((GlobalApplication)getApplication()).getKakaoName()+"");
         userName.append(((GlobalApplication)getApplication()).getKakaoName()+"님");
 //        Log.d("키 해쉬 : ", getKeyHash(this));
+
+        // ***** 카메라, 갤러리 *****
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("LOG", "권한 설정 완료");
+            } else {
+                Log.d("LOG", "권한 설정 요청");
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_camera:
+                        intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, TAKE_PICTURE);
+                        break;
+                }
+            }
+        });
+
+        btn_gallery.setOnClickListener(v -> {
+            switch (v.getId()) {
+                case R.id.btn_gallery:
+                    intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, GET_FROM_GALLERY);
+                    break;
+            }
+        });
 
 
 //        // PieChart 메소드
@@ -95,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.Camera:
                         View camera_pop = findViewById(R.id.camera_pop);
                         camera_pop.setVisibility(View.VISIBLE);
-                        camera_pop.bringToFront();
+//                        bloodBtn.setVisibility(View.GONE);
 //                    case R.id.Records:
 //                        final Intent intent3 = new Intent(StepCountChart.this, UserInfo.class);
 //                        startActivity(intent3);
@@ -127,5 +176,45 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        switch (requestCode) {
+            case TAKE_PICTURE: // 카메라로 촬영하는 경우
+                if (resultCode == RESULT_OK && intent.hasExtra("data")) {
+                    Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+//                    bitmap = bitmap.createScaledBitmap(bitmap,640,640,true);
+//                    if (bitmap != null) {
+//                       ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//                       float scale = (float) (1024/(float)bitmap.getWidth());
+//                       int image_w = (int) (bitmap.getWidth() * scale);
+//                       int image_h = (int) (bitmap.getHeight() * scale);
+//                       Bitmap resize = Bitmap.createScaledBitmap(bitmap, image_w, image_h, true);
+//                       resize.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+////                       byte[] byteArray = stream.toByteArray();
 
+                    Intent intent2 = new Intent(this, FoodRecognitionActivity.class);
+                    intent2.putExtra("image", bitmap);
+                    startActivity(intent2);
+                }
+                break;
+            case GET_FROM_GALLERY:
+                Log.e("DEBUG : result code is ", resultCode + "");
+                if (resultCode == RESULT_OK) {
+                    Uri uri = intent.getData();
+                    Log.e("DEBUG :  ", uri + "");
+                    GlideApp.with(getApplicationContext()).asBitmap().load(uri).override(300, 300).into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            Bitmap bitmap = resource;
+                            Log.e("DEBUG : ", bitmap + "");
+                            Intent intent2 = new Intent(MainActivity.this, FoodRecognitionActivity.class);
+                            intent2.putExtra("image", bitmap);
+                            startActivity(intent2);
+                        }
+                    });
+                }
+                break;
+        }
+    }
 }
